c1060fabeb2a758e1c0d8895364eac7d1673fb9d
Alsnj20 <marieljara656@gmail.com>
2023-12-29 23:50:31 -0500

    Prueba de una division de 2 nros funciona en terminal y en web

--- cgi-bin/calcu1.pl
+++ cgi-bin/calcu1.pl
@@ -6,13 +6,13 @@ print "Content-type: text/html\n\n";
 my $q = CGI->new;
 my $expression = $q->param('expression');
 if($expression){
-    if ($expression =~ /(\d+)\*(\d+)/) {
+    if ($expression =~ /(\d+)\/(\d+)/) {
         my $numero1 = $1;
         my $numero2 = $2;
-        my $resultado = $numero1 * $numero2;
-        print "Es un producto. El resultado es: ".$resultado;
+        my $resultado = $numero1 / $numero2;
+        print "Es una division. El resultado es: ".$resultado;
     } else {
-        print "No es un producto";
+        print "No es una division";
     }
 } else {
     print "Ingresa algo";
