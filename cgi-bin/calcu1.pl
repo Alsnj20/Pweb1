8761fd710706565946e1a9c5a6621138744cee7b
Alsnj20 <marieljara656@gmail.com>
2023-12-29 23:49:06 -0500

    Prueba de un producto de 2 nros funciona en terminal y en web

--- cgi-bin/calcu1.pl
+++ cgi-bin/calcu1.pl
@@ -6,13 +6,13 @@ print "Content-type: text/html\n\n";
 my $q = CGI->new;
 my $expression = $q->param('expression');
 if($expression){
-    if ($expression =~ /(\d+)\-(\d+)/) {
+    if ($expression =~ /(\d+)\*(\d+)/) {
         my $numero1 = $1;
         my $numero2 = $2;
-        my $resultado = $numero1 - $numero2;
-        print "Es una resta. El resultado es: $resultado";
+        my $resultado = $numero1 * $numero2;
+        print "Es un producto. El resultado es: ".$resultado;
     } else {
-        print "No es una resta";
+        print "No es un producto";
     }
 } else {
     print "Ingresa algo";
