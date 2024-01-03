4c8095da07e98b62e9e3ecad0ef6c0471ad6df77
Alsnj20 <marieljara656@gmail.com>
2024-01-03 08:22:26 -0500

    Add new file with the improve codification

--- Lab_09/cgi-bin/consult.pl
+++ Lab_09/cgi-bin/consult.pl
@@ -1,69 +1,72 @@
 #!C:/xampp/perl/bin/perl.exe
-binmode(STDOUT, ":utf8"); #perl entre el archivo con charset utf-8
+#perl entre el archivo con charset utf-8
 use strict;
 use warnings;
 use CGI;
 #cabecera de información
-print "Content-type: text/html\n\n";
+
+my $query = CGI->new;
+#my $fields = "CODIGO_ENTIDAD";
+#my $data = "065";
+
+my $fields = $query->param("fields");
+my $data = $query->param("data");
+$query->charset("UTF-8");
+print ($query->header("text/html"));
 print <<HTML;
 <!DOCTYPE html>
 <html lang="en">
     <head>
-    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <head> 
     <link rel="stylesheet" type="text/css" href="../estilo.css">
     <title>Búsqueda de Universidades Licenciadas</title>
-  </head>
+    </head>
 <body>
 HTML
-my $query = CGI->new;
-print "INGRESE VARIABLE";
-my $fields = <STDIN>;
-my $data = <STDIN>;
 
 my $flag;
-
 if(!($fields eq "") && !($data eq "")){
-open(IN, "../data/file.txt") or die "<h1>ERROR: open file</h1>\n";
+open(IN, "../data/Programas de Universidades.csv") or die "<h1>ERROR: open file</h1>\n";
     print "<p>Iniciando archivo</p>\n";
+    <IN>;
+    my %map = ("CODIGO_ENTIDAD" => 0, "NOMBRE" => 1, "PERIODO_LICENCIAMIENTO" => 2,
+    "DEPARTAMENTO_LOCAL" => 3, "DENOMINACION_PROGRAMA" => 4);
+    print "<table>\n";
+    print <<HTML;
+    <tr> 
+    <th>CODIGO ENTIDAD</th>
+    <th>NOMBRE</th>
+    <th>PERIODO DE LICENCIAMIENTO</th>
+    <th>DEPARTAMENTO LOCAL</th>
+    <th>DENOMINACIÓN PROGRAMA</th>
+    </tr>
+HTML
     while(my $line = <IN>){
-        print "$line\n";
-        my %list = isMatchLine($line);
-        print "$list{"$fields"}\n";
-        my $value = $list{$fields};
-        print "HOLA $value como estas\n";
-        if(defined($value) && $value =~ /.*$data.*/){
-            print "<h1>Encontrado:</h1>\n";
-            print "<p>$line</p>";
+        my @list = isMatchLine($line);
+        my $value = $list[$map{$fields}];
+        if($value && $value eq $data){
+            print "<tr>\n";
+            foreach my $n(@list){
+                print "<td>$n</td>\n";
+            }
+            print "</tr>\n";
             $flag = 1;
-            next;
         }
     }
     close(IN);
 }
-if(!defined($flag)){
+print "</table>\n";
+if(!$flag){
     print "<h1>No encontrado</h1>\n";
 }
+
 print <<HTML;
         <p>Ingrese <a href="../consulta.html">AQUI</a> para regresar al formulario de búsqueda</p>
     </body>
 </html>
 HTML
 
-sub isMatchLine {
-    my $line = $_[0];
-    my %dict;
-
-    if ($line =~ /^(\d+)\|([^|]+)\|.+/) {
-        $dict{"CODIGO_ENTIDAD"} = $1;
-        print "$1\n";
-        $dict{"name"} = $2;
-        print "$2\n";
-    }
-    return %dict;
-}
-#function for found query;
 #CODIGO_ENTIDAD|
 #NOMBRE|
 #TIPO_GESTION|ESTADO_LICENCIAMIENTO|
@@ -77,4 +80,14 @@ sub isMatchLine {
 #TIPO_NIVEL_ACADEMICO|NIVEL_ACADEMICO|CODIGO_CLASE_PROGRAMA_N2|
 #NOMBRE_CLASE_PROGRAMA_N2|TIPO_AUTORIZACION_PROGRAMA|
 #TIPO_AUTORIZACION_PROGRAMA_LOCAL
+#function for found query;
+sub isMatchLine {
+    my $line = $_[0];
+    my @arr;
+    if($line =~ /(.+?)\|(.+?)\|(?:.+?\|){2}(.+?)\|(?:.+?\|){5}(.+?)\|(?:.+?\|){5}(.+?)\|/){
+        @arr = ($1, $2, $3, $4, $5);
+    }
+    return @arr;
+}
+
 
